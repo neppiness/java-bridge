d8c9a157d889d499cf5c0d9ce258f035af17fd7a
neppiness <0414kjh@naver.com>
2022-12-07 18:03:40 +0900

    feat: InputView 기능 추가 (src/main/java/bridge)
    checkIfInputBlank 메소드 구현
    - 공백 문자 입력에 대한 예외 처리
    getInputLine 메소드 구현
    - 콘솔 입력 반환하는 메소드 구현. 반환 전에 공백 문자만으로 이루어진 입력이 아닌지 확인
    readMoving, readGameCommand 메소드 구현
    - getInputLine 메소드를 통해 콘솔 입력 반환
    readBridgeSize
    - 콘솔 입력을 정수로 파싱해서 반환

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -11,21 +11,28 @@ public class InputView {
      * 다리의 길이를 입력받는다.
      */
     public static int readBridgeSize() {
-        String lineInput = Console.readLine();
+        String lineInput = getInputLine();
         return Integer.parseInt(lineInput);
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
-    public static String readMoving() {
-        return null;
-    }
+    public static String readMoving() { return getInputLine(); }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public static String readGameCommand() {
-        return null;
+    public static String readGameCommand() { return getInputLine(); }
+
+    private static String getInputLine() {
+        String lineInput = Console.readLine();
+        checkIfInputBlank(lineInput);
+        return lineInput;
+    }
+
+    private static void checkIfInputBlank(String lineInput) {
+        if (lineInput.isBlank())
+            throw new IllegalArgumentException("공백 문자만을 입력할 수 없습니다.");
     }
 }
