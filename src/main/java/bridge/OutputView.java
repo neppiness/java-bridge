3fe5057123124d3d8d2ecea39ef1dc38e9478eb8
neppiness <0414kjh@naver.com>
2022-12-07 18:53:37 +0900

    feat: OutputView 클래스 메소드 수정 및 구현 (src/main/java/bridge)
    기능 1 수정 및 기능 4, 6 구현
    - 기능 1. 다리 길이 입력 안내: '다리의 길이를 입력해주세요. (3 이상 20 이하의 자연수)'
    - 기능 4. 이동 결과 출력
    - 기능 6. 게임 결과 출력

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -1,16 +1,62 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+
+    enum Level {
+        UPPERLEVEL("U"),
+        LOWERLEVEL("D");
+
+        String command;
+        Level (String command) { this.command = command; }
+    }
 
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printMap() {
+    public static void printMap(List<String> path, List<String> bridge) {
+        printLine(upperLevelMap(path, bridge));
+        printLine(lowerLevelMap(path, bridge));
+    }
+
+    private static String upperLevelMap(List<String> path, List<String> bridge) {
+        return mapAsString(path, bridge, Level.UPPERLEVEL.command);
+    }
+
+    private static String lowerLevelMap(List<String> path, List<String> bridge) {
+        return mapAsString(path, bridge, Level.LOWERLEVEL.command);
+    }
+
+    private static String mapAsString(List<String> path, List<String> bridge, String command) {
+        StringBuilder stringBuilder = new StringBuilder().append("[");
+        for (int partitionIndex = 0; partitionIndex < path.size(); partitionIndex++) {
+            String partitionMap = getPartitionMap(partitionIndex, path, bridge, command);
+            stringBuilder.append(partitionMap);
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+    private static String getPartitionMap(int partitionIndex, List<String> path, List<String> bridge, String command) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (partitionIndex != 0) stringBuilder.append("|");
+        String currentMove = path.get(partitionIndex);
+        String currentBridgePartition = bridge.get(partitionIndex);
+        String sign = getSign(currentMove, currentBridgePartition, command);
+        stringBuilder.append(" ").append(sign).append(" ");
+        return stringBuilder.toString();
+    }
+
+    private static String getSign(String currentMove, String currentBridgePartition, String command) {
+        if (!currentMove.equals(command)) return " ";
+        if (currentMove.equals(currentBridgePartition)) return "O";
+        return "X";
     }
 
     /**
@@ -18,7 +64,11 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public static void printResult() {
+    public static void printResult(boolean isSuccess, int numberOfTry) {
+        String passOrFail = "실패";
+        if(isSuccess) passOrFail = "성공";
+        printLine("게임 성공 여부: " + passOrFail);
+        printLine(String.format("총 시도한 횟수: %d", numberOfTry));
     }
 
     public static void announceStart() { printLine("다리 건너기 게임을 시작합니다."); }
@@ -27,7 +77,7 @@ public class OutputView {
 
     public static void guideMoveCommand() { printLine("이동할 칸을 선택해주세요. (위: U, 아래: D)"); }
 
-    public static void guideBridgeSize() { printLine("최종 게임 결과"); }
+    public static void guideBridgeSize() { printLine("다리의 길이를 입력해주세요. (3 이상 20 이하의 자연수)"); }
 
     public static void guideGameCommand() { printLine("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)"); }
 
